e1da9a14b31e4848f5bcd41b198f02cf67d0a8b9
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2026-08-17 22:15:06 +0100

    Build fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,29 +91,11 @@ const config = {
   					'border-right-color': 'black'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
   			typing: 'typing 1s steps(10), blink 1s infinite',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
   		},
   		fontFamily: {
   			satoshi: [
